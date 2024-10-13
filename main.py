61a4b081f79f819f360246795942508c3446444f
ErfanA002 <120725773+ErfanA002@users.noreply.github.com>
2024-10-13 15:00:12 +0330

    update to hst

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = {
-    'Basket': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-    'Apples': [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48],
-    'Bananas': [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32],
-    'Oranges': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-}
+import matplotlib.pyplot as plt 
+import numpy as np 
 
-df = pd.DataFrame(data)
+data = np.random.randn(1000)
 
-sum_apples = df['Apples'].sum()
-sum_bananas = df['Bananas'].sum()
-sum_oranges = df['Oranges'].sum()
+plt.hist(data, bins=30, edgecolor='black')
 
-x = ['Apples', 'Bananas', 'Oranges']
+plt.title('Histogram Example')
 
-y = [sum_apples, sum_bananas, sum_oranges]
+plt.xlabel('Values')
 
-plt.bar(x, y, color=['green', 'blue', 'Orange'])
-
-plt.title('Comparison of total Apples and Bananas and Oranges')
+plt.ylabel('Frequency') 
 
 plt.show()
+
