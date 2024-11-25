4267164e5878b38ed549d1ec8fdb2c0fdd0851fd
Lenovo <erfanasheri2002@gmail.com>
2024-11-25 17:06:19 +0330

    edite main file

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 img = cv2.imread('myimage.jpg',0)
 
-hist,bins = np.histogram(img.flatten(),256,[0,256])
+equ = cv2.equalizeHist(img)
 
-cdf = hist.cumsum()
-cdf_normalized = cdf * hist.max()/ cdf.max()
+res = np.hstack((img,equ))
 
-plt.plot(cdf_normalized, color = 'b')
-plt.hist(img.flatten(),256,[0,256], color = 'r')
-plt.xlim([0,256])
-plt.legend(('cdf','histogram'), loc = 'upper left')
-plt.show()
+cv2.imwrite('res.png',res)
